02978e1649462d5ed88a5bc323befe4aa4a412d6
rathandevaki <rathandevaki@gmail.com>
2022-07-14 23:01:57 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Haven  the PG app'),
+      home: const MyHomePage(title: 'Haven PG app'),
     );
   }
 }
